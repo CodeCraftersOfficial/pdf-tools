a3739b1afa399c7dd8c9ccc0544d0cbf4480c655
Code Crafters Official <askcodecrafters@gmail.com>
2025-10-28 11:17:00 +0530

    first

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('pdf-tool');
+    goTo(url: string) {
+    window.open(url, '_blank'); // Opens in a new tab
+  }
 }
